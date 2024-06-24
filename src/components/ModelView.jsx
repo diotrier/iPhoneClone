644abd10aa686b5da6444ae1c41fc2a988918690
Dio Trie Ramadhan <117532516+diotrier@users.noreply.github.com>
2024-06-24 14:57:11 +0700

    Update ModelView.jsx

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -49,11 +49,11 @@ const ModelView = ({
         position={[0, 0, 0]}
       >
         <Suspense fallback={<Loader />}>
-          <IPhone
+{/*           <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
             size={size}
-          />
+          /> */}
         </Suspense>
       </group>
     </View>
